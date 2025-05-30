ecde619d467676257ae92f697fe7664a144d4885
KrishnaM26 <krishna.mehta26@yahoo.com>
2025-05-30 13:26:19 -0400

    No note page

---
+++ frontend/src/components/NotesNotFound.jsx
@@ -0,0 +1,16 @@
+const NotesNotFound = () => {
+
+
+	return(
+
+	<div className = "flex flex-row pt-10 justify-center items-center">
+		<h3 className="text-2xl text-base-content/30">No Notes Found...</h3>
+	</div>
+	
+
+	);
+
+
+}
+
+export default NotesNotFound;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import NavBar from '../components/NavBar.jsx';
 import NoteCard from '../components/NoteCard.jsx';
+import NotesNotFound from '../components/NotesNotFound.jsx';
 import {useState, useEffect} from "react";
 import toast from "react-hot-toast";
 import api from "../lib/axios";
+
 
 const HomePage = () => {
 
@@ -20,7 +22,7 @@ const HomePage = () => {
 
 			}catch(err){
 				console.log("Error fetching notes");
-			}finally{
+		 	}finally{
 
 				setLoading(false);
 			}
@@ -36,6 +38,8 @@ const HomePage = () => {
 	<div className = "min-h-screen"> <NavBar />
 		<div className= "max-2-7xl mx-auto p-4 mt-6">
 		{loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+		{notes.length === 0 && <NotesNotFound />}
 		{notes.length > 0 && (
 			<div className = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
 				{notes.map(note => (
